38601f1e9c1f6c42fa369b204168903432923c83
Allen Roberts <allen.roberts1985@gmail.com>
2018-06-06 21:44:20 -0400

    Learning Contoller Scaffolding

---
+++ MVCLearn/MVCLearn/Data/MoviesContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVCLearn.Models
+{
+    public class MoviesContext : DbContext
+    {
+        public MoviesContext (DbContextOptions<MoviesContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVCLearn.Models.Movie> Movie { get; set; }
+    }
+}
